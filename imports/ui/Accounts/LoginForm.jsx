6ddffff67cbdf0ef2ce772df5f90dfe3d9718046
Bryan Reed <bryan87reed@gmail.com>
2021-08-07 09:09:20 -0700

    Login page design

--- imports/ui/Accounts/LoginForm.jsx
+++ imports/ui/Accounts/LoginForm.jsx
@@ -42,33 +42,48 @@ export const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={submit} className="loginForm">
-      <div>
-        <label htmlFor="username">Username</label>
+    <div className="loginContainer">
+      <form onSubmit={submit} className="loginFormContainer">
+        <div style={{ fontSize: 24 }}>
+          RedSky Innovations Journal Entry Tool
+        </div>
+        <div className="loginInnerContainer">
+          <div className="loginInputContainer">
+            <label className="loginText" htmlFor="username">
+              Username
+            </label>
 
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          required
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Username"
+              name="username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="loginInputContainer">
+            <label className="loginText" htmlFor="password">
+              Password
+            </label>
 
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit" className="mediumButton">
-        Log In
-      </button>
-      <div>{loginError}</div>
-    </form>
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="Password"
+              name="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="loginButton">
+            Log In
+          </button>
+          <div style={{ color: loginError ? "red" : "#fff" }}>
+            {loginError ? loginError : "Error message placeholder"}
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
