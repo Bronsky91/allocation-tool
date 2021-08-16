daa2a541b0a22cb267d658e584f44efae010b7fa
Bryan <bryan87reed@gmail.com>
2021-08-16 15:56:12 -0700

    New DB refactor whiteboarding

--- imports/api/MetricPublications.js
+++ imports/api/MetricPublications.js
@@ -4,3 +4,39 @@ import { MetricsCollection } from "../db/MetricsCollection";
 Meteor.publish("metrics", function publishMetrics() {
   return MetricsCollection.find({ userId: this.userId });
 });
+
+// user = {
+//   userId: "",
+//   name: "mary",
+//   admin: false,
+//   adminId: "denna42069", // user admin's ID
+//   permissions: {
+//     chartOfAccounts: ["coa_id", "2coa_id"],
+//     metrics: ["metricId", "metricId", "metricId", "metricId"],
+//     allocations: []
+//   },
+// };
+
+// chartOfAccounts = {
+//   name: "",
+//   userId: "", // Client Admin's User ID
+//   segments: [],
+//   templates: [],
+//   metrics = [
+//     {
+//       // ....
+//       allocations: [],
+//     },
+//   ],
+// };
+
+// const user = Meteor.user(); // mary's user account
+// const allChartOfAccounts = chartOfAccountsCollection.find({
+//   userId: user.adminId,
+// });
+// const availableChartOfAccounts = allChartOfAccounts.filter((coa) =>
+//   user.permission.chartOfAccounts.includes(coa._id)
+// );
+
+// const availableMetrics = allChartOfAccounts.metrics.filter((metric) =>
+//   user.permission.metrics.includes(metric))
